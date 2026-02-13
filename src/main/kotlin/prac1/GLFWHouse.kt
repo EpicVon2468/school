da9ee2c4a9d8dafb8f7e9f457d55cc810fc9e4da
EpicVon2468 <thomaschad0909@gmail.com>
2026-02-13 19:12:20 +1100

    Load fragment & vertex shader w/ getResourceAsStream()

--- src/main/kotlin/prac1/GLFWHouse.kt
+++ src/main/kotlin/prac1/GLFWHouse.kt
@@ -1,5 +1,18 @@
 package io.github.epicvon2468.school.prac1
 
+import org.glfw.glfw3_h.*
+
+import java.io.InputStream
+
 fun main() {
-	org.glfw.glfw3_h.NULL()
+	NULL()
+	println(FRAGMENT_SHADER)
+	println(VERTEX_SHADER)
 }
+
+private val implicitClass: Class<*> by lazy { Class.forName("io.github.epicvon2468.school.prac1.GLFWHouseKt") }
+
+val FRAGMENT_SHADER: String by lazy { getResource("/shader.frag") }
+val VERTEX_SHADER: String by lazy { getResource("/shader.vert") }
+
+fun getResource(name: String): String = implicitClass.getResourceAsStream(name)!!.use(InputStream::readAllBytes).decodeToString()
