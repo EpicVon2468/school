a429fcd39d3798ef539aea63fe32e79b7593b14e
EpicVon2468 <thomaschad0909@gmail.com>
2026-02-14 00:04:47 +1100

    copy most of NativeUtils from bunny, get GLFWHouse to make a window

--- src/main/kotlin/prac1/glfw/GLFWHouse.kt
+++ src/main/kotlin/prac1/glfw/GLFWHouse.kt
@@ -8,7 +8,8 @@ import java.lang.foreign.Arena
 import java.lang.foreign.MemoryLayout
 import java.lang.foreign.MemorySegment
 import java.lang.foreign.SequenceLayout
-import java.lang.invoke.VarHandle
+
+import kotlin.system.exitProcess
 
 val VEC2_LAYOUT: SequenceLayout = vecOf(2L)
 val VEC3_LAYOUT: SequenceLayout = vecOf(3L)
@@ -17,25 +18,67 @@ fun vecOf(count: Long): SequenceLayout = MemoryLayout.sequenceLayout(count, C_FL
 
 val verticesLayout: SequenceLayout = MemoryLayout.sequenceLayout(3L, Vertex.LAYOUT)
 // https://dev.java/learn/ffm/access-structure/
-val vertices: MemorySegment = Arena.global().allocate(verticesLayout).apply {
-	val vh: VarHandle = Vertex.LAYOUT.arrayElementVarHandle()
-	fun set(index: Long, value: Any) = vh.set(this, 0L, index, value)
-	val vertices = arrayOf(
-		Vertex(pos = arrayOf(-0.6f, -0.4f), col = arrayOf(1f, 0f, 0f)),
-		Vertex(pos = arrayOf(0.6f, -0.4f), col = arrayOf(0f, 1f, 0f)),
-		Vertex(pos = arrayOf(0f, 0.6f), col = arrayOf(0f, 0f, 1f))
-	)
-	set(0L, vertices[0].delegate)
-	set(1L, vertices[1].delegate)
-	set(2L, vertices[2].delegate)
-}
+// FIXME: Apparently I also gave up on Vertex in KMP_GE;  I'll have to use the other method for now, but hopefully I'll figure this out.
+//val vertices: MemorySegment = Arena.global().allocate(verticesLayout).apply {
+//	val vh: VarHandle = Vertex.LAYOUT.arrayElementVarHandle()
+//	fun set(index: Long, value: Any) = vh.set(this, 0L, index, value)
+//	val vertices = arrayOf(
+//		Vertex(pos = arrayOf(-0.6f, -0.4f), col = arrayOf(1f, 0f, 0f)),
+//		Vertex(pos = arrayOf(0.6f, -0.4f), col = arrayOf(0f, 1f, 0f)),
+//		Vertex(pos = arrayOf(0f, 0.6f), col = arrayOf(0f, 0f, 1f))
+//	)
+//	set(0L, vertices[0].delegate)
+//	set(1L, vertices[1].delegate)
+//	set(2L, vertices[2].delegate)
+//}
+val vertices = Arena.global().allocateArray(
+	vecOf(9L),
+	0.0f, 0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f,
+	is2DArray = true
+)
 
 // https://docs.oracle.com/en/java/javase/25/core/foreign-function-and-memory-api.html
+// https://github.com/EpicVon2468/KMP_GE/blob/master/src/nativeMain/kotlin/io/github/epicvon2468/kmp_ge/core/main.kt
 fun main() {
 	NULL()
 	Vertex(pos = arrayOf(1f, 2f), col = arrayOf(3f, 4f, 5f))
 	println(FRAGMENT_SHADER)
 	println(VERTEX_SHADER)
+//	vertices
+//	vecOf(9L).arrayElementVarHandle(MemoryLayout.PathElement.sequenceElement()).get(vertices, 0L, 4L, 0L).let {
+//		println("Got it: $it")
+//	}
+	if (glfwInit() != 1) {
+		println("ERROR - Failed to initialise GLFW!")
+		exitProcess(1)
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR(), 4)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR(), 6)
+	glfwWindowHint(GLFW_OPENGL_PROFILE(), GLFW_OPENGL_CORE_PROFILE())
+
+	val window = glfwCreateWindow(640, 480, "My House".cstr(Arena.global()), MemorySegment.NULL, MemorySegment.NULL)
+	if (window == MemorySegment.NULL) {
+		println("ERROR - GLFWWindow failed to create!")
+		glfwTerminate()
+		exitProcess(1)
+	}
+
+	glfwMakeContextCurrent(window)
+
+	glfwSwapInterval(1)
+
+	while (glfwWindowShouldClose(window) != 1) {
+		glfwSwapBuffers(window)
+		glfwPollEvents()
+	}
+
+	glfwDestroyWindow(window)
+	glfwTerminate()
+
+	exitProcess(0)
 }
 
 private val implicitClass: Class<*> by lazy { Class.forName("io.github.epicvon2468.school.prac1.glfw.GLFWHouseKt") }
---
+++ src/main/kotlin/prac1/glfw/NativeUtils.kt
@@ -0,0 +1,113 @@
+package io.github.epicvon2468.school.prac1.glfw
+
+// Sourced from: https://github.com/EpicVon2468/bunny/blob/master/src/jvmMain/kotlin/io/github/epicvon2468/bunny/NativeUtils.kt
+
+import java.lang.foreign.Arena
+import java.lang.foreign.MemoryLayout
+import java.lang.foreign.MemorySegment
+import java.lang.foreign.SequenceLayout
+import java.lang.invoke.VarHandle
+
+/**
+ * Copies the contents of this [Collection] into a native C-style array using [allocateArray].
+ *
+ * @receiver the [Collection] to copy values from.
+ *
+ * @param arena the [Arena] to use for allocation.
+ * @param elementLayout the layout of the [Collection]'s values.
+ *
+ * @return a [MemorySegment] pointing to the allocated native C-style array.
+ *
+ * @author Mavity The Madity
+ *
+ * @see allocateArray
+ * @see Arena
+ * @see MemorySegment
+ * @see MemoryLayout
+ * @see MemoryLayout.sequenceLayout
+ * @see MemoryLayout.arrayElementVarHandle
+ */
+fun Collection<Any?>.toNativeArray(
+	arena: Arena,
+	elementLayout: MemoryLayout
+): MemorySegment = toTypedArray().toNativeArray(arena, elementLayout)
+
+/**
+ * Copies the contents of this [Array] into a native C-style array using [allocateArray].
+ *
+ * @receiver the [Array] to copy values from.
+ *
+ * @param arena the [Arena] to use for allocation.
+ * @param elementLayout the layout of the [Array]'s values.
+ *
+ * @return a [MemorySegment] pointing to the allocated native C-style array.
+ *
+ * @author Mavity The Madity
+ *
+ * @see allocateArray
+ * @see Arena
+ * @see MemorySegment
+ * @see MemoryLayout
+ * @see MemoryLayout.sequenceLayout
+ * @see MemoryLayout.arrayElementVarHandle
+ */
+fun Array<Any?>.toNativeArray(
+	arena: Arena,
+	elementLayout: MemoryLayout
+): MemorySegment = arena.allocateArray(elementLayout, *this)
+
+/**
+ * Allocates a native C-style array of [values], using [elementLayout] to define the array's layout.
+ *
+ * @receiver the [Arena] to use for allocation.
+ *
+ * @param elementLayout the layout of [values].
+ * @param values the values of the new array.
+ *
+ * @return a [MemorySegment] pointing to the allocated native C-style array.
+ *
+ * @author Mavity The Madity
+ *
+ * @see Arena
+ * @see MemorySegment
+ * @see MemoryLayout
+ * @see MemoryLayout.sequenceLayout
+ * @see MemoryLayout.arrayElementVarHandle
+ */
+fun Arena.allocateArray(elementLayout: MemoryLayout, vararg values: Any?, is2DArray: Boolean = false): MemorySegment {
+	val layout: SequenceLayout = MemoryLayout.sequenceLayout(values.size.toLong(), elementLayout)
+	val array: MemorySegment = allocate(layout)
+	val vh: VarHandle = if (is2DArray) elementLayout.arrayElementVarHandle(MemoryLayout.PathElement.sequenceElement()) else elementLayout.arrayElementVarHandle()
+	for (current: Long in 0..<layout.elementCount()) vh.set(array, 0L, current, 0L, values[current.toInt()])
+	return array
+}
+
+fun MemorySegment.jvmNull(): MemorySegment? = if (this == MemorySegment.NULL) null else this
+fun MemorySegment?.nativeNull(): MemorySegment = this ?: MemorySegment.NULL
+
+// This always evaluates 'other' (even if inlined)
+infix fun MemorySegment.elvis(other: MemorySegment): MemorySegment = this.jvmNull() ?: other
+
+// This version does not have the same problem, but might not always want braces
+infix fun MemorySegment.elvis(other: () -> MemorySegment): MemorySegment = this.jvmNull() ?: other()
+
+@JvmField
+val EMPTY_STRING: MemorySegment = Arena.global().allocateFrom("")
+
+/**
+ * Allocates a native C-style string from [this], returning [EMPTY_STRING] if [this] is [null or empty][isNullOrEmpty].
+ *
+ * @param arena the [Arena] to use for allocation (Note: [EMPTY_STRING] is already allocated by [Arena.global]).
+ *
+ * @return a [MemorySegment] pointing to the allocated native C-style string.
+ *
+ * @author Mavity The Madity
+ *
+ * @see String
+ * @see Arena
+ * @see Arena.allocateFrom
+ * @see MemorySegment
+ * @see EMPTY_STRING
+ * @see isNullOrEmpty
+ */
+fun String?.cstr(arena: Arena): MemorySegment = if (isNullOrEmpty()) EMPTY_STRING else arena.allocateFrom(this)
