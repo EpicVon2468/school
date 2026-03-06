4a07d0de4613d0b9cbf42544e63fc034379cfdff
EpicVon2468 <thomaschad0909@gmail.com>
2026-03-07 10:35:24 +1100

    note

--- src/main/kotlin/prac3/SecondsToHMS.kt
+++ src/main/kotlin/prac3/SecondsToHMS.kt
@@ -8,6 +8,7 @@ fun main() {
 	val time: Long = input?.toLongOrNull() ?: throw IllegalArgumentException("Invalid duration: '$input'!")
 	time.seconds.toComponents { days: Long, hours: Int, minutes: Int, seconds: Int, _: Int ->
 		val actualHours: Long = hours + (days * 24)
+		// I know the criterion says "use printf to display the result", but that's (normally) inefficient in Kotlin since string templates are a language feature
 		println("$time seconds == $actualHours hours, $minutes minutes, and $seconds seconds")
 	}
 }
