28eeecb1843cc211f75ce0507db6c04e2204af7f
EpicVon2468 <thomaschad0909@gmail.com>
2026-03-25 23:52:59 +1100

    move font

--- src/main/kotlin/prac5/extension/CalculatorLookAndFeel.kt
+++ src/main/kotlin/prac5/extension/CalculatorLookAndFeel.kt
@@ -24,18 +24,21 @@ class CalculatorLookAndFeel : NimbusLookAndFeel() {
 			metalDefaults.filter { it.key.toString().startsWith("Scroll") }
 		)
 		this.fixText()
-		// Overrides the font to be JetBrains Mono Light :)
-		val size: Int = (uiDefaults[FONT_KEY] as Font).size
-		var font = Font(FONT, Font.PLAIN, size)
-		// Font wasn't available by default (non-JBR?)
-		if (font.family != FONT) font = Font(Font.MONOSPACED, Font.PLAIN, size)
-		uiDefaults[FONT_KEY] = FontUIResource(font)
+		uiDefaults[FONT_KEY] = FONT
 	}
 
 	companion object {
 
-		private const val FONT_KEY = "defaultFont"
-		const val FONT = "JetBrains Mono Light"
+		private const val FONT_KEY: String = "defaultFont"
+		private const val FONT_NAME: String = "JetBrains Mono Light"
+		@JvmStatic
+		private val FONT: FontUIResource = run {
+			val size: Int = System.getProperty("mavity.calculator.fontsize", "13").toInt()
+			var font = Font(FONT_NAME, Font.PLAIN, size)
+			// Fallback
+			if (font.family != FONT_NAME) font = Font(Font.MONOSPACED, Font.PLAIN, size)
+			FontUIResource(font)
+		}
 
 		// I've set the default L&F to be CalculatorLookAndFeel in my Gradle script, but if run out of a JAR or if the property is ever changed it may not remain set
 		// Some pracs may want the Calc L&F to always be on, and a short one-liner is preferable to doing this check individually many times
